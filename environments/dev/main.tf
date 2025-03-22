3e4c98b22f3c84840eeab31b6c01263612f09144
Hwangjh94 <9401hyun@naver.com>
2025-03-22 14:41:01 +0900

    Initial commit: Terraform Security Gatekeeper setup

---
+++ environments/dev/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "mybuckettest94"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
+}
+
+module "aws_config" {
+  source = "../../modules/security-checks"
+  
+  environment = "dev"
+}
+
+# 여기에 실제 인프라 리소스 추가
